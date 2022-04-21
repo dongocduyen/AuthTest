0f04637188fd9ff57d4436e0ed8e75ed9d52fd6f
dongo <dongo@NGOC-DUYEN>
2022-04-21 11:09:08 +0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AuthTest.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthTest.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Staff> Staffs { get; set; }
+    }
+}
---
+++ Models/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthTest.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; } 
+        [Display(Name="Tên của nhân viên")]
+        public string Name { get; set; }
+        public int StaffId { get; set; }
+        public string Department { get; set; }
+        public DateTime CreatedDate { get; set; }
+        
+
+        //
+        // public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
